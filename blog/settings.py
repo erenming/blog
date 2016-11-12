bc397bacdafc95399d580aabbd44040da0a4257d
tomming <tomming233@gmail.com>
2016-11-12 10:20:13 +0800

    update

--- blog/settings.py
+++ blog/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ("/home/tom/django/blog/app/static",)
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 
 # Settings for django-bootstrap3
