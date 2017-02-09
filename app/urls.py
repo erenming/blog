faa08e417ac7cdecabcd77d16859daa0dbfc98ca
tomming233 <tomming233@gmail.com>
2017-02-09 13:45:29 +0800

    添加redis作为缓存数据库

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
+from django.views.decorators.cache import cache_page
 from . import views
 
 app_name = 'app'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     # (?P<article_id>\d+)为一个组, 其中?P<article_di>中的article_id代表了该组的组名
-    url(r'^article/(?P<article_id>\d+)$', views.ArticleDetailView.as_view(), name='detail'),
+    url(r'^article/(?P<article_id>\d+)$', cache_page(60 * 15)(views.ArticleDetailView.as_view()), name='detail'),
     url(r"^category/(?P<cate_id>\d+)$", views.CategoryView.as_view(), name='category'),
     url(r'^article/(?P<article_id>\d+)/comment/$', views.CommentView, name='comment'),
     url(r'^search/$', views.blog_search, name='search'),
--- blog/settings.py
+++ blog/settings.py
@@ -31,6 +31,18 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# cache settings
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+
 
 # Application definition
 
